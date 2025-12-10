955cddfb99f8ad36acf452ec14af4c126668222a
abijoserah <serahabijo94@gmail.com>
2025-12-10 23:20:29 +0100

    fetching api

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  const [cats, setCats] = useState([])
+  useEffect(() => {
+    async function loadCats() {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search?include_breeds=1&limit=100")
+      const catsFromApi = await response.json()
+      console.log(catsFromApi)
+      setCats(catsFromApi)
+    }
+    loadCats()
+  }, [])
+
+  return (
+    <>
+      {cats.map((cat) => (
+        <img src={cat.url} alt='A cat photo' />
+      ))}
+    </>
+  )
+}
+
+export default App
